4b81acf029d5e2b3c6c2361f24efea1ca2c54e96
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-03-26 11:51:00 +0600

    header add

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+           <h1 className='header-title'><span className='color-custom-tilte'>Oracal</span> teaching care</h1>
+        </div>
+    );
+};
+
+export default Header;
