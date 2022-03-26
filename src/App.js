7ed857d41c57431f0629e3d8ca9489586b4a6775
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-03-26 12:46:44 +0600

    fakedb loaded on console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Courses from './Components/Courses/Courses';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Courses></Courses>
     </div>
   );
 }
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Courses = () => {
+    const [courses,setCourses] = useState([]);
+
+
+    useEffect(()=>{
+        fetch('coursesdb.json')
+        .then(res =>res.json())
+        .then(data => setCourses(data))
+    })
+
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='row'>
+            <div className="course-container col-lg-10">
+                <div className='row'>
+                        {
+                            courses.map(course=> console.log(course))
+                        }
+                </div>
+            </div>
+            <div className="cart-container col-lg-2">
+                This is cart area
+            </div>
+            </div>
         </div>
     );
 };
